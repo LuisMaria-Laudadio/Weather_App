6057d50a35e59391d7421d767c63c7192c115e04
LuisMaria-Laudadio <130412817+LuisMaria-Laudadio@users.noreply.github.com>
2025-07-10 16:26:18 -0300

    Update main.js
    mal ruteado el img  forecast-item-temp

--- main.js
+++ main.js
@@ -134,7 +134,7 @@ function updateForecastsItems(weatherData) {
     const forecastItem =`
         <div class="forecast-item">
             <h5 class="forecast-item-data regular-txt">${dateResult}</h5>
-            <img src="/assets/weather/${getWeatherIcon(id)}" class="forecast-item-img"/>
+            <img src="assets/weather/${getWeatherIcon(id)}" class="forecast-item-img"/>
             <h5 class="forecast-item-temp">${Math.round(temp)} °C</h5>
         </div> `
         
